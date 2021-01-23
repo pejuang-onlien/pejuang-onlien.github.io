0795cf7ef4a0d19b5f090345d7ef6d101b1e6666
hikigaya <hikigaya@hikigaya-haciman>
2021-01-23 15:57:19 +0700

    Update index.html

--- initpost.sh
+++ initpost.sh
@@ -46,7 +46,7 @@ POSTPATH="${BINPATH}/_posts"
 DIST_FOLDER="$POSTPATH"
 
 # Set your blog URL
-BLOG_URL="https://jekflix.rossener.com/"
+BLOG_URL="https://pejuang-onlien.github.io/"
 
 # Set your assets URL
 ASSETS_URL="assets/img/"
